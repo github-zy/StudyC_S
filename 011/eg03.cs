bb8f452045247d098f6a5195ce05ab53b7c7a461
github-zy <843019013@qq.com>
2026-01-21 20:06:17 +0800

    Add 011/eg03.cs

---
+++ 011/eg03.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //delegate匿名函数
+            this.myButton.Click += delegate(object sender, RoutedEventArgs e)
+            {
+                this.myTextBox.Text = "Hello, World!";
+            };
+            //lamda表达式
+            this.myButton.Click += (sender, e)=>
+            {
+                this.myTextBox.Text = "Hello, World!";
+            };
+        }
+    }
+}
